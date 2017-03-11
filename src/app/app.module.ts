1fd2912db4409ac8139d9b3a1b9f19ad57efa197
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-03-11 13:57:07 -0500

    Adds route for heroes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
+
+
 
 @NgModule({
   declarations: [
@@ -17,7 +20,13 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   providers: [
     HeroService
